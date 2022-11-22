06bab1ebfc56d38f1f28fca449aacefd338d1835
valeriyarv07 <103383160+valeriyarv07@users.noreply.github.com>
2022-11-22 08:31:02 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1> Weather App</h1>
       <footer>
         This project was coded by Valeriia Rybakova and is {" "}
         <a href="https://github.com/valeriyarv07/weather-react-5" target="_blank">open-source code on GitHub</a>
