84b6d8bf66bd60bb0740d1c5aa789dc71bbb02f0
valeriyarv07 <103383160+valeriyarv07@users.noreply.github.com>
2022-11-22 10:45:17 +0200

    update weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,21 @@ import React from 'react';
 import "./Weather.css"
 
 export default function Weather (){
-    return <div>Hello from Weather</div>
+    return (
+        <div className="whole-page-wrapper">
+            <div className="card container-whole-page">
+                <div className="card-body">
+                    <form id="search-form">
+                        <div className="row input-group">
+                            <div className="col-sm-auto">
+                                <input>
+                                </input>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+
 }
