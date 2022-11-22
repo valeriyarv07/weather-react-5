b17df2e32a7cd7cc748ac00deff198131c538c1f
valeriyarv07 <103383160+valeriyarv07@users.noreply.github.com>
2022-11-22 08:36:34 +0200

    create weather js and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Weather">
       <h1> Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Valeriia Rybakova and is {" "}
         <a href="https://github.com/valeriyarv07/weather-react-5" target="_blank">open-source code on GitHub</a>
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import "./Weather.css"
+
+export default function Weather (){
+    return <div>Hello from Weather</div>
+}
