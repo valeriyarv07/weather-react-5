2c3e49389fd7609e2295dbec04f68e1fa764fe06
valeriyarv07 <103383160+valeriyarv07@users.noreply.github.com>
2022-12-09 23:30:52 +0200

    forecast for 5 days in a given city

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import { Collapse } from "bootstrap";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css"
 import axios from "axios";
@@ -8,6 +8,12 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded,setLoaded] = useState(false);
   let [forecast,setForecast] = useState(null);
+
+  useEffect(() => {
+  setLoaded(false);
+  }, [props.coordinates])
+
+
 
   function handleResponce(response) {
     console.log(response.data);
